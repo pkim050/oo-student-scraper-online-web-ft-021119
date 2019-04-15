e0d3e775311c70c0a17a4c08d4d4ed0caef70e7c
Patrick Kim <pkim050@gmail.com>
2019-04-15 05:54:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |key, value|
+<<<<<<< HEAD
       temp = key.to_s
       temp += "="
       self.send(temp, student_hash[key])
@@ -27,5 +28,21 @@ class Student
 
   def self.all
     @@all
+=======
+      @key = value
+    end
+  end
+
+  def self.create_from_collection(students_array)
+
+  end
+
+  def add_student_attributes(attributes_hash)
+
+  end
+
+  def self.all
+
+>>>>>>> f531902b54084f4526da91cc5553ed6c2922a75a
   end
 end
